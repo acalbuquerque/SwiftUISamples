bca9200cb5c1d108f7903ec02273aebb72d166e5
Antonio Netto <tonis.04@gmail.com>
2023-02-19 16:35:50 -0300

    bullseye first commit

---
+++ BullseyeApp/Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Antonio Netto on 19/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BullseyeApp/Bullseye/Bullseye/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Antonio Netto on 19/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            VStack {
+                Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                Text("89")
+                HStack {
+                    Text("1")
+                    Slider(value: .constant(50), in: 1.0...100.0)
+                    Text("100")
+                }
+                Button("Hit me") {
+                    print("hit")
+                }
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
